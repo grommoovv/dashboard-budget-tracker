c0a4dbf02a965884fbbce09877341b6d0854bf29
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-06-24 20:00:12 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware } from '@clerk/nextjs'
 
-export default clerkMiddleware()
+export default authMiddleware({})
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
